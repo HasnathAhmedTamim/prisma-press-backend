cdac785a81b107197c12d20dd4698962e5c6528f
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-07-04 22:59:40 +0600

    user register with prisma in modular pattern

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-
 import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
 import cors from "cors";
+import httpStatus from "http-status";
+import { userRoutes } from "./modules/users/user.route";
 
 const app: Application = express();
 
@@ -17,9 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
+// app.post()
 
+app.use("/api/users", userRoutes);
 export default app;
